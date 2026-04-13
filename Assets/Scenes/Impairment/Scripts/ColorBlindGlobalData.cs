8fe29d0a0dd7ea0f11c2dcc34615161158e32a19
leucht <lucas.philipp@st.ovgu.de>
2026-04-13 14:30:08 +0200

    Cleanup and Preparation for local shader implementation

---
+++ Assets/Scenes/Impairment/Scripts/ColorBlindGlobalData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ColorBlindGlobalData", menuName = "Scriptable Objects/ColorBlindGlobalData")]
+public class ColorBlindGlobalData : ScriptableObject
+{
+    
+}
